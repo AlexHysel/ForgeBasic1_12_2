790050565b83e1299a0e6427d0b757c585d3902c
AlexHysel <alexhysel@gmail.com>
2025-09-25 11:48:15 +0300

    1

--- src/main/java/com/example/modid/ExampleMod.java
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -1,7 +1,8 @@
 package com.example.modid;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,14 +12,22 @@ public class ExampleMod {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
-    /**
-     * <a href="https://cleanroommc.com/wiki/forge-mod-development/event#overview">
-     *     Take a look at how many FMLStateEvents you can listen to via the @Mod.EventHandler annotation here
-     * </a>
-     */
+    //FMLPreInitializationEvent: fires when Forge is ready to start initializing mods,
+    //you can again query annotations once again, and see where different files related to your mod would situate (e.g. config files).
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+        LOGGER.info("{} preInitialization", Tags.MOD_NAME);
     }
 
+    //FMLInitializationEvent: fires after registry events are fired, game objects largely is available in this event.
+    //Hence a lot of OreDictionary activity is done here.
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event){
+        LOGGER.info("{} Initialization", Tags.MOD_NAME);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+        LOGGER.info("{} postInitialization", Tags.MOD_NAME);
+    }
 }
